911b3071562660abcdc84e72b1648950ad60b22f
Laura Adamson <adamsn14@aol.com>
2014-12-03 19:10:16 -0700

    Previous work

---
+++ Assets/Scripts/flight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flight : MonoBehaviour {
+	public virtual void OnMouseEnter()
+	{
+		StaticVars.gameScore++;
+		Object.Destroy (this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/falling.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class falling : MonoBehaviour {
+	//void OnMouseEnter (){
+	//	DuckFlying.IsPlaying("falling");
+	//	}
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
